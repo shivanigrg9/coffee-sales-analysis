0fe1bf8cf65f25f8ab6cf6cb872b463886ca5008
Shivani Garg <88229758+shivanigrg9@users.noreply.github.com>
2025-12-02 19:50:51 +0530

    Update coffee_queries.sql

--- sql/coffee_queries.sql
+++ sql/coffee_queries.sql
@@ -1,1 +1,16 @@
+-- Total revenue
+SELECT SUM(total_price) AS total_revenue
+FROM coffee_sales_2025;
+
+-- Top selling items
+SELECT item, SUM(quantity) as total_qty
+FROM coffee_sales_2025
+GROUP BY item
+ORDER BY total_qty DESC;
+
+-- Daily revenue trend
+SELECT date, SUM(total_price) as revenue
+FROM coffee_sales_2025
+GROUP BY date
+ORDER BY date;
 
